8f42dbfe5562f04cfa2e4005351db814a99b96c6
salmaehab <salmaiihab@gmail.com>
2023-01-13 19:36:46 +0200

    Added merge sorted array solution

---
+++ mergeSortedArray.js
@@ -0,0 +1,10 @@
+var merge = function(nums1, m, nums2, n) {
+    let j=0;
+    for(let i=m;i<m+n;i++)
+    {
+        nums1[i]= nums2[j]
+        j++
+    }
+   return nums1.sort((a, b) => a - b)
+};
+console.log(merge([1,2,3,0,0,0],3, [2,5,6],3))
